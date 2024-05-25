a2bd5c67a77c4ec97090d035497351055f5c7f11
goktugmandaci <goktugmandaci@sabanciuniv.edu>
2024-05-25 10:56:27 +0300

    frontend update

--- frontend/src/components/InsertProduct.js
+++ frontend/src/components/InsertProduct.js
@@ -1,8 +1,43 @@
-import React from 'react'
+
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const InsertProduct = () => {
+
+  const [_name, setName] = useState("");
+  const [_desription, setDescription] = useState("")
+  const [_price, setPrice] = useState(-1);
+
+  const submitHandler = async () => {
+    e.preventDefault()
+    try {
+      const requestBody = { name: _name, description: _desription, price: _price }
+      const response = await axios.post('http://localhost:8080/products/create', requestBody)
+      console.log(response.data)
+      window.location.reload()
+    } catch (error) {
+      console.log("Error inserting product: ", error)
+    }
+  }
+
   return (
-    <div>InsertProduct</div>
+    <div className='App'>
+      <form onSubmit={() => submitHandler()}>
+        <div class="form-group">
+          <label for="name">Product Name</label>
+          <input onChange={(e) => setName(e.target.value)} type="text" class="form-control" id="name" aria-describedby="name" placeholder="Product Name" />
+        </div>
+        <div class="form-group">
+          <label for="description">Description</label>
+          <input onChange={(e) => setDescription(e.target.value)} type="text" class="form-control" id="description" placeholder="Description" />
+        </div>
+        <div class="form-group">
+          <label for="price">Price</label>
+          <input onChange={(e) => setPrice(parseInt(e.target.value))} type="number" class="form-control" id="price" placeholder="Price" />
+        </div>
+        <button type="submit" class="btn btn-primary">Insert product</button>
+      </form>
+    </div>
   )
 }
 
--- frontend/src/components/InsertUser.js
+++ frontend/src/components/InsertUser.js
@@ -1,8 +1,38 @@
-import React from 'react'
+
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const InsertUser = () => {
+
+  const [_username, setUsername] = useState("");
+  const [_email, setEmail] = useState("")
+
+  const submitHandler = async () => {
+    e.preventDefault()
+    try {
+      const requestBody = { username: _username, email: _email }
+      const response = await axios.post('http://localhost:7070/users/create', requestBody)
+      console.log(response.data)
+      window.location.reload()
+    } catch (error) {
+      console.log("Error inserting user: ", error)
+    }
+  }
+
   return (
-    <div>InsertUser</div>
+    <div className='App'>
+      <form onSubmit={() => submitHandler()}>
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input onChange={(e) => setUsername(e.target.value)} type="text" class="form-control" id="username" aria-describedby="username" placeholder="Username" />
+        </div>
+        <div class="form-group">
+          <label for="email">E-Mail</label>
+          <input onChange={(e) => setEmail(e.target.value)} type="text" class="form-control" id="email" placeholder="Email" />
+        </div>
+        <button type="submit" class="btn btn-primary">Insert User</button>
+      </form>
+    </div>
   )
 }
 
